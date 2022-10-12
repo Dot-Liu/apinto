679e6607db5f2348fd0bc9be00e4a4e8dd706afb
Liujian <824010343@qq.com>
2022-10-12 17:30:41 +0800

    修改打包脚本

--- build/cmd/package.sh
+++ build/cmd/package.sh
@@ -4,12 +4,11 @@
 
 cd ${BasePath}/
 
-
 VERSION=$(genVersion $1)
 folder="${BasePath}/out/apinto-${VERSION}"
 if [[ ! -d "$folder" ]]
 then
-  mkdir "$folder"
+  mkdir -p "$folder"
   ${CMD}/build.sh $1
   if [[ "$?" != "0" ]]
   then
